63fd8ced4a55e125f5604df2c45413e1ec51e3f2
Anton <anton@Edgar.(none)>
2013-02-20 14:48:04 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+gimp-android-icon-gen
+
+This is a Python-Fu Gimp plugin designed to make it easy to generate output icon resources for your android project. The plugin takes your current visible image in Gimp and generates web-, xhdpi-, hdpi- and mdpi- png assets in the proper android folder structure. With this plugin, you can iteratively tweak and test the icon for your android app without having to export multiple files or drag and drop or rename anything.
+
+To install, just put 'androidResourceGen.py' in your '.gimp-X.X/plug-ins' folder and restart Gimp. Make sure you have Gimp Python support enabled.
+
+
---
+++ androidResourceGen.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python
+
+from gimpfu import *
+import io
+import os
+
+def python_android_icon_generator(img, layer, wrap, outDirectory, outname, websz, xhdpisz, hdpisz, mdpisz):
+	if wrap: 
+		filename = os.path.basename(img.filename).split(".")[0] #gets file name for use as directory
+		outDirectory = os.path.join(outDirectory, filename)	
+		
+	'''
+	The wrap option allows the user to generate output inside a directory 
+	(e.g. if they want to generate icons and email them to a someone else).
+	By disabling the wrap option and selecting the root folder of an 
+	Android source project, the user can directly overwrite the drawables
+	of the project, allowing effortless updating of icons.
+	''' 
+	
+	
+	
+	sizes = { #map sizes to icon dimensions (icons are square, we only
+				"web": websz,
+				"xhdpi": xhdpisz,
+				"hdpi": hdpisz,
+				"mdpi": mdpisz
+				}
+
+	outpaths = {#map sizes to output folder structures
+				"web":   outDirectory,
+				"xhdpi": os.path.join(outDirectory, "res", "drawable-xhdpi"),
+				"hdpi":  os.path.join(outDirectory, "res", "drawable-hdpi"),
+				"mdpi":  os.path.join(outDirectory, "res", "drawable-mdpi")
+				}
+	
+	for size, res in sizes.items():
+		fileNameAugment = "-web" if size == "web" else ""
+		saveScaledImage(img, outpaths[size], outname + fileNameAugment, res)
+	return;
+# end
+
+
+	
+def saveScaledImage(image, filepath, filename, res):
+	
+	
+	if not os.path.exists(filepath):
+		os.makedirs(filepath)
+		
+	fullpath = os.path.join(filepath, filename + ".png")
+	
+	scaledImage = gimp.Image(image.width, image.height, RGB)#new image to copy original to
+	
+	layer = pdb.gimp_layer_new_from_visible(image, scaledImage, 'final')#merge visible to new, unassociated layer
+	scaledImage.add_layer(layer)
+	layer.scale(res, res, 0) 
+	scaledImage.resize(res, res, 0, 0)
+	
+	pdb.file_png_save_defaults(scaledImage, layer, fullpath, fullpath)
+		# save to file
+
+# end 
+
+
+
+
+###################### setup ##########################################
+
+defaultPath = os.path.join(os.path.expanduser("~"), "Desktop");
+defaultPath = defaultPath if os.path.exists(defaultPath) else os.path.expanduser("~")
+
+
+register(
+        "python_fu_icon_gen",
+        "Android icon output generator",
+        "Generates android target files for mdpi, hdpi, xhdpi and web in proper Android file structures (res/drawable-hdpi, res/drwable-mdpi etc...)",
+        "Anton Lodder",
+        "Anton Lodder",
+        "2013",
+        "<Image>/Android/Generate Icon Set...",
+        "RGB*, GRAY*",
+        [
+		(PF_TOGGLE, "wrap", "Wrap output in a folder?", True),
+		(PF_DIRNAME, "filepath", "Output Filepath", defaultPath),
+        (PF_STRING, "filename", "Android Resource Name", "ic_launcher"),
+        (PF_INT16, "websz", "Dimension - web:", 512),
+        (PF_INT16, "xhdpisz", "Dimension - xhdpi:", 96),
+        (PF_INT16, "hdpisz", "Dimension - hdpi:", 72),
+        (PF_INT16, "mdpisz", "Dimension - mdpi:", 48),
+		],
+		[],
+        python_android_icon_generator)
+
+main()
+'''
+
+		
+
+'''
